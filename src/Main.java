a313a7052c89805e0c46acdfea50f13398c2b8b0
Lyer42 <senamostaev@gmail.com>
2024-07-20 00:23:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+//        циклы
+       // for (float i = 100; i > 10; i /= 2) {
+           //System.out.println("Element: " + i);
+       // }
+
+      //  int i = 0;
+     //   while(i < 10) {
+       //     System.out.println("Element: " + i);
+         //   i++;
+
+       // int i = 100;
+       // do {
+         //   System.out.println("element: " + i);
+          //  i *= 100;
+
+        //} while(i < 10);
+
+
+        for(int i = 5; i < 25; i += 2) {
+            if(i % 3 == 0)
+                continue;
+
+            if(i >= 17)
+                break;
+
+            System.out.println("element: " + i);
+        }
+    }
+}
